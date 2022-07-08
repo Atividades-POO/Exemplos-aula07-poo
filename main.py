c0cc9186ca51396056a93fbe766369b906b30e81
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-08 10:39:29 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#
+#
+import ex01
+
+valor = input("Digite um valor: ")
+
+print(ex01.tratamento_int(valor))
+print(ex01.tratamento_float(valor))
+print(ex01.tratamento_string(valor))
+print(ex01.tratamento_list(valor))
+print(ex01.tratamento_tuple(valor))
+print(ex01.tratamento_dict(valor))
+print(ex01.tratamento_set(valor))
